3e15034c8ddb086474170e9e9623d77d46fd7aa3
Johan Yrefors <joyr0993@arrhenius.it.uu.se>
2021-05-04 14:40:38 +0200

    Some updates with timeing of functions

--- OU4/ou4_2/fibonacci.py
+++ OU4/ou4_2/fibonacci.py
@@ -3,5 +3,11 @@
 import ctypes
 lib = ctypes.cdll.LoadLibrary('./libfibonacci.so')
 
-def fib(n):
+def fib_cpp(n):
 	return lib.fib_c(n)
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return fib_py(n-1)+fib_py(n-2)
--- OU4/ou4_2/ou4_2_fib.py
+++ OU4/ou4_2/ou4_2_fib.py
@@ -1,11 +1,29 @@
+
 #!/usr/bin/env python3
 
-from fibonacci import fib
+from fibonacci import fib_cpp, fib_py
+from time import perf_counter as pc
 
 def main():
+	rng = 40
+	print(f"Range: {rng}")
+	tic = pc()
+	for n in range(rng):
+		#print(fib_cpp(n))
+		fib_cpp(n)
+	toc = pc()
+	time_cpp = toc-tic
 
-	for n in range(10):
-		print(fib(n))
+	tic = pc()
+	for n in range(rng):
+		#print(fib_py(n))
+		fib_py(n)
+	toc = pc()
+	time_py = toc-tic
+
+	print(f"Calculations of fib(n) for n=0 to n={rng}")
+	print(f"Time C++: {time_cpp:0.2f}")
+	print(f"Time python: {time_py:0.2f}")
 
 if __name__ == '__main__':
 	main()
