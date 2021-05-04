abd3e6fe0e738e52c6bebdb8dfc87076dfb7b8d3
Johan Yrefors <joyr0993@arrhenius.it.uu.se>
2021-05-04 16:29:09 +0200

    Time comparisons and plotting added

--- OU4/ou4_2/heltal.cpp
+++ OU4/ou4_2/heltal.cpp
@@ -2,12 +2,16 @@
 // Integer class 
 
 class Heltal{
+	private:
+		int val;
+		int pfib(int n);
 	public:
 		Heltal(int);
 		int get();
 		void set(int);
-	private:
-		int val;
+		int fib(){
+			return pfib(val); 
+		}
 	};
  
 Heltal::Heltal(int n){
@@ -21,7 +25,14 @@ int Heltal::get(){
 void Heltal::set(int n){
 	val = n;
 	}
-	
+
+int Heltal::pfib(int n){
+	if (n <= 1){
+		return n;
+	} else {
+		return pfib(n-1)+pfib(n-2);
+	}
+}
 
 extern "C"{
 	Heltal* Heltal_new(int n) {return new Heltal(n);}
@@ -33,4 +44,5 @@ extern "C"{
 			heltal = nullptr;
 			}
 		}
+	int Heltal_fib(Heltal* heltal) {heltal->fib();}
 	}
--- OU4/ou4_2/ou4_2.py
+++ OU4/ou4_2/ou4_2.py
@@ -1,12 +1,40 @@
 #!/usr/bin/env python3
 
 from heltal import Heltal
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return fib_py(n-1)+fib_py(n-2)
 
 def main():
-	f = Heltal(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+	rng = list(range(30,35))
+
+	py_times = []
+	cpp_times = []
+
+	for n in rng:
+		tic = pc()
+		fib_py(n)
+		toc = pc()
+		py_times.append(round(toc-tic,2))
+
+	f = Heltal(0)
+	for n in rng:
+		f.set(n)
+		tic = pc()
+		f.fib()
+		toc = pc()
+		cpp_times.append(round(toc-tic,2))
+
+	print(f"Python times: {py_times}")
+	print(f"C++ times: {cpp_times}")
+
+	plt.plot(rng,py_times,rng,cpp_times)
+	plt.savefig('fib_times.pdf')
 
 if __name__ == '__main__':
 	main()
